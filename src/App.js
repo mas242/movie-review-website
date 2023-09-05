44a97b4668e206f80c02e9f509bc385f70e4dcd9
mas242 <mattsmith533811@yahoo.com>
2023-09-05 04:01:44 -0600

    adding in movie poster display component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import MovieList from "./components/MovieList";
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      Title: "Star Wars: Episode II - Attack of the Clones",
+      Year: "2002",
+      imdbID: "tt0121765",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
+    },
+    {
+      Title: "Rogue One: A Star Wars Story",
+      Year: "2016",
+      imdbID: "tt3748528",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MovieList movies={movies} />
     </div>
   );
 }
