1f934f4df91d4e17ce79ee421f2aed784320127b
mas242 <mattsmith533811@yahoo.com>
2023-09-10 03:50:16 -0600

    added in favorites row and functionality to add and remove movies from favorites. uses localstorage to keep track of favorites list and also downgraded to bootstrap 4.0.0 for the UI

--- src/App.js
+++ src/App.js
@@ -1,33 +1,84 @@
 import logo from "./logo.svg";
-import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFav from "./components/AddFav";
+import RemoveFav from "./components/RemoveFav";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Star Wars: Episode II - Attack of the Clones",
-      Year: "2002",
-      imdbID: "tt0121765",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
-    },
-    {
-      Title: "Rogue One: A Star Wars Story",
-      Year: "2016",
-      imdbID: "tt3748528",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=675cf7f0`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  useEffect(() => {
+    const movieFavorites = JSON.parse(
+      localStorage.getItem("react-movie-app-favorites")
+    );
+
+    setFavorites(movieFavorites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favorites", JSON.stringify(items));
+  };
+
+  const addFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
 
   return (
-    <div className='container-fluid movie-app'>
-      <div className='row'>
-        <MovieList movies={movies} />
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        ></SearchBox>
+      </div>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          handleFavoriteClick={addFavoriteMovie}
+          favoriteComponent={AddFav}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavoriteClick={removeFavoriteMovie}
+          favoriteComponent={RemoveFav}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 const MovieList = (props) => {
-    return (
-        <>
-            {props.movies.map((movie, index) => 
-                <div>
-                    <img src={movie.Poster} alt='movie'></img>
-                </div>)}
-        </>
-    )
-}
+    const FavoriteComponent = props.favoriteComponent;
+
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m-3">
+          <img src={movie.Poster} alt="movie"></img>
+          <div onClick={()=>props.handleFavoriteClick(movie)} className="overlay d-flex align-items-center justified-content-center">
+            <FavoriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default MovieList;
